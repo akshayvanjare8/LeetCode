d8b349c2504be2ad07d371bdf38571dbe6e4cd67
Akshay Nivrutti Vanjare <72245329+akshayvanjare8@users.noreply.github.com>
2023-05-04 13:08:39 +0530

    Add files via upload

---
+++ Database/177. Nth Highest Salary/177. Nth Highest Salary.sql
@@ -0,0 +1,24 @@
+Create table If Not Exists Employee (Id int, Salary int);
+
+Truncate table Employee;
+
+insert into Employee (id, salary) values ('1', '100');
+
+insert into Employee (id, salary) values ('2', '200');
+
+insert into Employee (id, salary) values ('3', '300');
+
+
+-- Solution
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT salary
+      FROM Employee 
+      ORDER BY salary DESC
+      LIMIT 1 OFFSET N
+  );
+END
