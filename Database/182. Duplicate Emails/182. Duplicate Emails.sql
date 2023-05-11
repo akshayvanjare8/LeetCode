586e096a19a85b56b95ce0154df1e19d7586510a
Akshay Nivrutti Vanjare <72245329+akshayvanjare8@users.noreply.github.com>
2023-05-11 18:29:16 +0530

    Add files via upload

---
+++ Database/182. Duplicate Emails/182. Duplicate Emails.sql
@@ -0,0 +1,18 @@
+Create table If Not Exists Person (id int, email varchar(255));
+
+Truncate table Person;
+
+insert into Person (id, email) values ('1', 'a@b.com');
+
+insert into Person (id, email) values ('2', 'c@d.com');
+
+insert into Person (id, email) values ('3', 'a@b.com');
+
+-- Solution
+
+# Write your MySQL query statement below
+
+SELECT email as Email 
+FROM Person 
+GROUP BY email 
+HAVING count(*) > 1;
